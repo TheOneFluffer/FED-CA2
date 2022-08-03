e6bbfe3eabf1496e4fbd5b129e4cca6cc51b1c83
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2022-08-04 00:58:04 +0800

    I like Keebe

--- Javascript/Feedback.js
+++ Javascript/Feedback.js
@@ -2,3 +2,13 @@ function Submission()
     {
         alert("Your feedback form has been submitted!");
     }
+
+function disable()
+{
+    document.getElementById("myRadio").disabled = true;
+}
+      
+function undisable()
+{
+    document.getElementById("myRadio").disabled = false;
+}
