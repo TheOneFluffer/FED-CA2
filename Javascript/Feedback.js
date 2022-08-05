8598e04431b7666b31adf287ee3cdb79517864e9
TheOneFluffer <90469659+TheOneFluffer@users.noreply.github.com>
2022-08-05 22:25:23 +0800

    Final update

--- Javascript/Feedback.js
+++ Javascript/Feedback.js
@@ -1,14 +1,4 @@
 function Submission()
-    {
-        alert("Your feedback form has been submitted!");
-    }
-
-function disable()
 {
-    document.getElementById("myRadio").disabled = true;
-}
-      
-function undisable()
-{
-    document.getElementById("myRadio").disabled = false;
+    alert("Your feedback form has been submitted!");
 }
